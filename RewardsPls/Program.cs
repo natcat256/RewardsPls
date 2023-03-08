f7e5b26e3497dcc05872a943ff2a49bc7c6e01b9
Nat <95653800+natcat256@users.noreply.github.com>
2023-03-08 16:01:15 -0500

    Initial commit

---
+++ RewardsPls/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+
+namespace RewardsPls
+{
+    internal class Program
+    {
+        static string GenerateRandomString(int length)
+        {
+            const string characterSet = "abcdefghijklmnopqrstuvwxyz1234567890";
+            string result = "";
+
+            Random random = new Random();
+
+            for (int i = 0; i < length; i++)
+            {
+                int index = random.Next(0, characterSet.Length - 1);
+                result += characterSet[index];
+            }
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Title = "RewardsPls";
+
+            string epuid;
+            try
+            {
+                epuid = File.ReadAllText("EPuid.txt");
+            }
+            catch
+            {
+                Console.WriteLine("EPuid.txt could not be read. This file must be accessible and must contain your Rewards account's EPuid.");
+                goto Exit;
+            }
+
+            WebClient webClient = new WebClient();
+
+            try
+            {
+                dynamic stats = Rewards.ReportActivity(webClient, epuid, "");
+                Console.WriteLine("Current balance: {0}", (int)stats["RewardsSessionData"]["Balance"]);
+
+                bool level2 = (bool)stats["RewardsSessionData"]["IsLevel2"];
+                Console.WriteLine("Is level 2: {0}", level2);
+
+                Console.WriteLine("Acquiring daily points...");
+
+                if (level2)
+                {
+                    for (int i = 0; i < 150; i += 5)
+                    {
+                        if (i < 20)
+                            stats = Rewards.ReportActivity(webClient, epuid, GenerateRandomString(20), Rewards.UserAgent.Edge);
+
+                        if (i < 100)
+                            stats = Rewards.ReportActivity(webClient, epuid, GenerateRandomString(20), Rewards.UserAgent.Android);
+
+                        stats = Rewards.ReportActivity(webClient, epuid, GenerateRandomString(20), Rewards.UserAgent.Firefox);
+                    }
+                }
+                else
+                {
+                    stats = Rewards.ReportActivity(webClient, epuid, GenerateRandomString(20), Rewards.UserAgent.Edge);
+
+                    for (int i = 0; i < 50; i += 5)
+                    {
+                        stats = Rewards.ReportActivity(webClient, epuid, GenerateRandomString(20), Rewards.UserAgent.Firefox);
+                    }
+                }
+
+                Console.WriteLine("New balance: {0}", (int)stats["RewardsSessionData"]["Balance"]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred while generating Rewards points: {0}", ex.Message);
+            }
+
+        Exit:
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ RewardsPls/Rewards.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using Newtonsoft.Json.Linq;
+
+namespace RewardsPls
+{
+    internal static class Rewards
+    {
+        public static class UserAgent
+        {
+            public const string Firefox = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/110.0";
+            public const string Edge = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36 Edge/15.15063";
+            public const string Android = "Mozilla/5.0 (Linux; Android 12) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.71 Mobile Safari/537.36";
+        }
+
+        public static JObject ReportActivity(WebClient webClient, string epuid, string searchQuery, string userAgent = UserAgent.Firefox)
+        {
+            string searchUrl = "https://www.bing.com/search?q=" + searchQuery + "&V=web";
+
+            webClient.Headers["Cookie"] = "_RwBf=e=" + epuid;
+            webClient.Headers["User-Agent"] = userAgent;
+            webClient.Headers["Content-Type"] = "application/x-www-form-urlencoded";
+
+            string response = webClient.UploadString("https://www.bing.com/rewardsapp/reportActivity", 
+                "url=" + WebUtility.UrlEncode(searchUrl));
+
+            const int prologueLength = 151;
+            const int epilogueLength = 61;
+
+            if (response.Length <= (prologueLength + epilogueLength))
+                throw new Exception("Invalid response");
+
+            string json = response.Remove(response.Length - epilogueLength)
+                .Remove(0, prologueLength);
+
+            return JObject.Parse(json);
+        }
+    }
+}
